e69baee6555c13c11a95afad0ddc437242762bc4
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-18 03:23:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,6 +12,8 @@ def monopoly_with_third_tier
   hash = monopoly_with_second_tier
   hash[:railroads][:rent_in_dollars][:one_piece_owned] = 25
   hash[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
+  hash[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
+  hash[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
   # = {:pieces => 4, 
   #  rent_in_dollars => {
   #      :one_piece_owned => 25,
