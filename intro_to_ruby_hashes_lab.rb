fd20ebee0cc4a3bf239351ef75a16a4b2df18771
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-18 03:41:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  hash = monopoly_with_second_tier
+hash = monopoly_with_second_tier
   hash[:railroads][:rent_in_dollars][:one_piece_owned] = 25
   hash[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
   hash[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
@@ -23,10 +23,10 @@ end
 
 def monopoly_with_fourth_tier
   hash = monopoly_with_third_tier
-  hash[:railroads][:names][:reading_railroad] = {}
-  hash[:railroads][:names][:pennsylvania_railroad] = {}
-  hash[:railroads][:names][:b_and_o_railroad] = {}
-  hash[:railroads][:names][:shortline_railroad] = {}
+    hash[:railroads][:names][:reading_railroad][:mortgage_value] = 100
+    hash[:railroads][:names][:pennsylvania_railroad][:mortgage_value] = 200
+    hash[:railroads][:names][:b_and_o_railroad][:mortgage_value] = 400
+    hash[:railroads][:names][:shortline_railroad][:mortgage_value] = 800
   
   
   # When you start writing the implementation for this method, copy the Hash
